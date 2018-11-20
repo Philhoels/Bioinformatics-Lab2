5e2085d7afbbe8ac577bdc8870d9b4bc6940e827
Thijs Quast <tquast@MacBook-Pro-van-Thijs.local>
2018-11-20 22:11:19 +0100

    loop to count stop codons per sequence

--- Question2/rscript_question_2.R
+++ Question2/rscript_question_2.R
@@ -1,1 +1,17 @@
 # Question 2
+# 2.1 
+#install.packages("stringr")
+library(stringr)
+string <- "acctgctcaactc"
+
+taa_count <- c()
+tag_count <- c()
+tga_count <- c()
+
+for (i in 1:33){
+string <- sequences[[1]]
+taa_count[i] <-str_count(string, pattern = "taa")
+tag_count[i] <- str_count(string, pattern = "tag")
+tga_count[i] <- str_count(string, pattern = "tga")
+
+}
