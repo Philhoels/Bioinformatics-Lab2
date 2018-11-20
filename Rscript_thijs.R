c89b60890af1ba76ee46224519d9e65e8ab8cc36
Thijs Quast <tquast@c-10-253-219-120.clients.eduroam.liu.se>
2018-11-20 11:49:10 +0100

    question 1.2

--- Rscript_thijs.R
+++ Rscript_thijs.R
@@ -1,1 +1,33 @@
 # Script from Thijs
+#install.packages("ape")
+library(ape)
+#install.packages("seqinr")
+library(seqinr)
+getwd()
+lizards_sequences <- read.fasta("lizard_seqs.fasta")
+
+tree <- rtree(n = 33)
+plot(tree, edge.width = 2)
+
+
+#install.packages("phangorn")
+library(phangorn)
+transition_matrix <- matrix(0.25, ncol = 4, nrow = 4)
+rownames(transition_matrix) <- c("a", "c", "t", "g")
+colnames(transition_matrix) <- c("a", "c", "t", "g")
+transition_matrix
+lengths <- c()
+
+for (i in 1:33){
+lengths <- c(lengths, length(lizards_sequences[[i]]))
+}
+
+lengths
+sequences_artificial <- list()
+
+for (j in 1:33){
+sequences_artificial[j] <- simSeq(tree, l = lengths[j], Q=transition_matrix , type = "DNA")
+}
+
+abc <- simSeq(tree, l = 100, Q=transition_matrix , type = "DNA")
+
