e00479572ffe22de7726f2201395142433cccd02
Lennart Schilling <lensc874@student.liu.se>
2018-11-20 13:17:09 +0100

    update q_1_1

--- Question1/Question1.R
+++ Question1/Question1.R
@@ -1,57 +1,52 @@
 library(seqinr)
 
-# reading fasta file 
-lizards_sequences = read.fasta("lizard_seqs.fasta")
-
-# data preparation (dna sequences include emtpy spaces which will be removed)
-for (i in 1:length(lizards_sequences)) {
-  lizards_sequences[[i]] = lizards_sequences[[i]][lizards_sequences[[i]] != " "]
-}
-
-# Question 1.1
-get_artificial_sequence_dataset = function(original_dataset) {
-  # creating empty varibales which will be filled in following for loop
-  original_sequence_distributions = list()
+# function for simulating and saving artificial sequence dataset and comparing base compositions to original dataset
+write_artificial_sequence_dataset = function(original_dataset) {
+  # creating empty varibales which will be filled in following for-loop
+  original_base_compositions = list()
   artificial_dataset = list()
-  artificial_sequence_distributions = list()
-  a_original = c()
-  c_original = c()
-  g_original = c()
-  t_original = c()
-  a_artificial = c()
-  c_artificial = c()
-  g_artificial = c()
-  t_artificial = c()
+  artificial_base_compositions = list()
+  a_original = c(); c_original = c(); g_original = c(); t_original = c()
+  a_artificial = c(); c_artificial = c(); g_artificial = c(); t_artificial = c()
   for (i in 1:length(original_dataset)) {
-    # getting original sequence distribution for each list element
-    original_sequence_distributions[[i]] = seqinr::count(original_dataset[[i]],1)/length(original_dataset[[i]])
-    # creating artificial dna sequences 
+    # getting base compositions for each original sequence
+    original_base_compositions[[i]] = seqinr::count(original_dataset[[i]],1)/length(original_dataset[[i]])
+    # creating artificial sequences randomly drawn from the distribution given by the base composition
     artificial_dataset[[as.character(i)]] = sample(x = c("a","c","g","t"),
                                                    size = length(original_dataset[[i]]),
                                                    rep = TRUE,
-                                                   prob = original_sequence_distributions[[i]])
-    # creating data.frame to compare nucleotide distributions between original and artificial sequences 
-    artificial_sequence_distributions[[i]] = seqinr::count(artificial_dataset[[i]],1)/length(artificial_dataset[[i]])
-    a_original = c(a_original, round(original_sequence_distributions[[i]][1],2))
-    a_artificial = c(a_artificial, round(artificial_sequence_distributions[[i]][1],2))
-    c_original = c(c_original, round(original_sequence_distributions[[i]][2],2))
-    c_artificial = c(c_artificial, round(artificial_sequence_distributions[[i]][2],2))
-    g_original = c(g_original, round(original_sequence_distributions[[i]][3],2))
-    g_artificial = c(g_artificial, round(artificial_sequence_distributions[[i]][3],2))
-    t_original = c(t_original, round(original_sequence_distributions[[i]][4],2))
-    t_artificial = c(t_artificial, round(artificial_sequence_distributions[[i]][4],2))
+                                                   prob = original_base_compositions[[i]])
+    # creating dataframe to compare base compositions between original and artificial sequences 
+    artificial_base_compositions[[i]] = seqinr::count(artificial_dataset[[i]],1)/length(artificial_dataset[[i]])
+    a_original = c(a_original, round(original_base_compositions[[i]][1],2))
+    a_artificial = c(a_artificial, round(artificial_base_compositions[[i]][1],2))
+    c_original = c(c_original, round(original_base_compositions[[i]][2],2))
+    c_artificial = c(c_artificial, round(artificial_base_compositions[[i]][2],2))
+    g_original = c(g_original, round(original_base_compositions[[i]][3],2))
+    g_artificial = c(g_artificial, round(artificial_base_compositions[[i]][3],2))
+    t_original = c(t_original, round(original_base_compositions[[i]][4],2))
+    t_artificial = c(t_artificial, round(artificial_base_compositions[[i]][4],2))
   }
-  comparison_sequence_distributions = cbind(
+  comparison_base_compositions = cbind(
     name_original = names(original_dataset), name_artificial = names(artificial_dataset),
     a_original, a_artificial, c_original, c_artificial, g_original, g_artificial, t_original, t_artificial
   )
-  # comparison_sequence_distributions = round(comparison_sequence_distributions, 2)
-  print("comparison of sequence distributions between original and artificial datasets (values rounded): ")
-  print(comparison_sequence_distributions)
+  rownames(comparison_base_compositions) = 1:nrow(comparison_base_compositions)
+  print("comparison of base compositions between original and artificial datasets (values rounded): ")
+  print(comparison_base_compositions)
   # saving fasta file
   ape::write.dna(artificial_dataset, file ="artificial_dataset.fasta", format = "fasta")
-  # returning artificial dna sequences
-  return(artificial_dataset)
 }
-artificial_sequences = get_artificial_sequence_dataset(lizards_sequences)
+
+# applying function to dataset of the RAG1 gene sequences from 33 lizard species
+
+  # reading original_dataset from fasta file 
+  lizards_sequences = read.fasta("lizard_seqs.fasta")
+
+  # preparing data in fasta file (dna sequences include emtpy spaces which will be removed)
+  for (i in 1:length(lizards_sequences)) {
+    lizards_sequences[[i]] = lizards_sequences[[i]][lizards_sequences[[i]] != " "]
+  }
+
+  write_artificial_sequence_dataset(lizards_sequences)
 
