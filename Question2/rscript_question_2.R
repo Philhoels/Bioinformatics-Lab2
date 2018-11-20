ad022a9e6c209ad450c3ddbc5ec5ac9f867d5ba1
Thijs Quast <tquast@MacBook-Pro-van-Thijs.local>
2018-11-20 22:38:44 +0100

    markov model

--- Question2/rscript_question_2.R
+++ Question2/rscript_question_2.R
@@ -13,5 +13,21 @@ string <- sequences[[1]]
 taa_count[i] <-str_count(string, pattern = "taa")
 tag_count[i] <- str_count(string, pattern = "tag")
 tga_count[i] <- str_count(string, pattern = "tga")
-
 }
+
+
+# 2.2 Markov chain
+nucleotide <- c("a", "c", "g", "t")
+
+trans_matrix <- matrix(0.25, ncol = 4, nrow = 4)
+rownames(trans_matrix) <- c("a", "c", "g", "t")
+colnames(trans_matrix) <- c("a", "c", "g", "t")
+trans_matrix
+
+#install.packages("markovchain")
+library(markovchain)
+
+sequenceMatr <- createSequenceMatrix(sequence, sanitize = FALSE)
+sequences <- list(x = c("a", "b", "a"), y = c("b", "a", "b", "a", "c"))
+mcFitMle <- markovchainFit(sequences, method = "mle")
+mcFitMle
